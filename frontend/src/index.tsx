ebacb29cbb00f800a8fc0e1dd92cc90945aa24ec
Youri Gruiters <i-youri-i@hotmail.com>
2022-04-21 21:19:57 +0200

    Setup Redux with Redux toolkit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './app/app';
+import { Provider } from 'react-redux';
 
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/redux/store.js';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('app'),
 );
